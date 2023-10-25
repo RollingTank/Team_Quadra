dd33f1a59a06033305baaf0d533ff559cf066b8a
Tanush Kulkarni <kultanush@gmail.com>
2023-10-24 21:10:56 -0400

    Updated Code for Autonomous

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ConceptTensorFlowObjectDetection.java
@@ -29,13 +29,10 @@
 
 package org.firstinspires.ftc.teamcode;
 
-import android.util.Size;
-
-import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.checkerframework.checker.units.qual.A;
-import org.firstinspires.ftc.teamcode.AutoMove;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
@@ -51,14 +48,24 @@ import java.util.List;
  * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
  * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list.
  */
-@Autonomous(name = "TwishaIsOPP")
-public class TF_Auto2 extends LinearOpMode {
+@TeleOp(name = "Concept: TensorFlow Object Detection", group = "Concept")
+//@Disabled
+public class ConceptTensorFlowObjectDetection extends LinearOpMode {
 
     private static final boolean USE_WEBCAM = true;  // true for webcam, false for phone camera
     private static final String TFOD_MODEL_ASSET = "model_20231018_181921.tflite";
     private static final String[] LABELS = {
             "team object",
     };
+
+    // TFOD_MODEL_ASSET points to a model file stored in the project Asset location,
+    // this is only used for Android Studio when using models in Assets.
+    // TFOD_MODEL_FILE points to a model file stored onboard the Robot Controller's storage,
+    // this is used when uploading models directly to the RC using the model upload interface.
+    //private static final String TFOD_MODEL_FILE = "/sdcard/FIRST/tflitemodels/myCustomModel.tflite";
+    // Define the labels recognized in the model for TFOD (must be in training order!)
+
+
     /**
      * The variable to store our instance of the TensorFlow Object Detection processor.
      */
@@ -72,39 +79,38 @@ public class TF_Auto2 extends LinearOpMode {
     @Override
     public void runOpMode() {
 
-        waitForStart();
-
         initTfod();
-        AutoMove m = new AutoMove();
 
         // Wait for the DS start button to be touched.
+        telemetry.addData("DS preview on/off", "3 dots, Camera Stream");
+        telemetry.addData(">", "Touch Play to start OpMode");
         telemetry.update();
+        waitForStart();
 
-
-
+        if (opModeIsActive()) {
+            while (opModeIsActive()) {
 
                 telemetryTfod();
 
-
                 // Push telemetry to the Driver Station.
                 telemetry.update();
-                List<Recognition> currentRecognitions = tfod.getFreshRecognitions();
-                while(currentRecognitions.size() == 0){
-                    m.rotate();
-                    currentRecognitions = tfod.getFreshRecognitions();
+
+                // Save CPU resources; can resume streaming when needed.
+                if (gamepad1.dpad_down) {
+                    visionPortal.stopStreaming();
+                } else if (gamepad1.dpad_up) {
+                    visionPortal.resumeStreaming();
                 }
-                m.moveForward();
-                m.dropPixel();
-                m.stopMoving(); 
+
                 // Share the CPU.
                 sleep(20);
             }
-        
+        }
 
         // Save more CPU resources when camera is no longer needed.
-        //visionPortal.close();
+        visionPortal.close();
 
-       // end runOpMode()
+    }   // end runOpMode()
 
     /**
      * Initialize the TensorFlow Object Detection processor.
@@ -114,11 +120,16 @@ public class TF_Auto2 extends LinearOpMode {
         // Create the TensorFlow processor by using a builder.
         tfod = new TfodProcessor.Builder()
 
-            // Use setModelAssetName() if the TF Model is built in as an asset.
-            // Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
+            // With the following lines commented out, the default TfodProcessor Builder
+            // will load the default model for the season. To define a custom model to load, 
+            // choose one of the following:
+            //   Use setModelAssetName() if the custom TF Model is built in as an asset (AS only).
+            //   Use setModelFileName() if you have downloaded a custom team model to the Robot Controller.
             .setModelAssetName(TFOD_MODEL_ASSET)
             //.setModelFileName(TFOD_MODEL_FILE)
 
+            // The following default settings are available to un-comment and edit as needed to 
+            // set parameters for custom models.
             .setModelLabels(LABELS)
             //.setIsModelTensorFlow2(true)
             //.setIsModelQuantized(true)
@@ -138,10 +149,10 @@ public class TF_Auto2 extends LinearOpMode {
         }
 
         // Choose a camera resolution. Not all cameras support all resolutions.
-        builder.setCameraResolution(new Size(640, 480));
+        //builder.setCameraResolution(new Size(640, 480));
 
         // Enable the RC preview (LiveView).  Set "false" to omit camera monitoring.
-        //builder.enableCameraMonitoring(true);
+        //builder.enableLiveView(true);
 
         // Set the stream format; MJPEG uses less bandwidth than default YUY2.
         //builder.setStreamFormat(VisionPortal.StreamFormat.YUY2);
